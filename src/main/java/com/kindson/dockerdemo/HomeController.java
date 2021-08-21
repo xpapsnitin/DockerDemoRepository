b320008a44db34965f112e83f9f8b45cf8812944
Swamiji <nitinpatilat@gmail.com>
2021-08-21 16:41:24 +0530

    first commit

---
+++ src/main/java/com/kindson/dockerdemo/HomeController.java
@@ -0,0 +1,19 @@
+package com.kindson.dockerdemo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	private String hello() {
+		
+		return "Hello";
+	}
+
+}
