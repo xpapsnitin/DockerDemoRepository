532de07e849d6a122113fc0d5562099cc362320b
Swamiji <nitinpatilat@gmail.com>
2021-08-25 19:14:30 +0530

    Docker demo app file changed

--- src/main/java/com/kindson/dockerdemo/HomeController.java
+++ src/main/java/com/kindson/dockerdemo/HomeController.java
@@ -1,9 +1,5 @@
 package com.kindson.dockerdemo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
